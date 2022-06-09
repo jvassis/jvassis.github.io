6d6c7013a4aa9a52f1085c685056d5c4c12e0706
Jackson Assis <jackson.assis07@gmail.com>
2022-06-09 16:07:03 -0300

    alteracao da biblioteca de icones

---
+++ script.js
@@ -0,0 +1,8 @@
+const menu_mobile = document.getElementById('mobile-menu')
+
+function toogleMenu() {
+    const nav = document.getElementById('nav')
+    nav.classList.toggle('active')
+}
+
+menu_mobile.addEventListener('click', toogleMenu)
