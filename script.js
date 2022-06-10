154c32452c46adcef2222e70c1c6e4ce504f2048
Jackson Assis <jackson.assis07@gmail.com>
2022-06-10 12:03:13 -0300

    estilizacao do menu hamburger

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const menu_mobile = document.getElementById('mobile-menu')
 function toogleMenu() {
     const nav = document.getElementById('nav')
     nav.classList.toggle('active')
+    menu_mobile.classList.toggle('active')
 }
 
 menu_mobile.addEventListener('click', toogleMenu)
